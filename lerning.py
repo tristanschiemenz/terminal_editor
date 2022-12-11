88b73e461af422e5557e65e324054a3d57f8ee2c
schimiHD1 <tristanschiemenz@gmail.com>
2022-12-11 21:21:14 +0100

    Adding a raw menu design and working selectioin system

--- lerning.py
+++ lerning.py
@@ -1,52 +1,57 @@
-"""
 import curses
 from curses import wrapper
 import time
+
+WELCOME_TEXT = "Welcome to my Terminal-Based-Text-Editor"
+FUNCTION_LIST = ["pass","pass","pass"]
 
 #addstr - postiony,postionx,text,attributes
 #init_pair(farb sets) - channel,foreground color, backgroundcolor
 #stdscr.addstr(10,10,"Hello, World!",curses.color_pair(1) | curses.A_UNDERLINE)
 
-def main(stdscr):
-    curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLUE)
-    GREEN_AND_BLUE = curses.color_pair(1)
+
+def menu(stdscr):
+    rows, cols = stdscr.getmaxyx()
+    center_y, center_x = rows // 2, cols // 2
+    #first two rows
+    stdscr.addstr(0,center_x-(len(WELCOME_TEXT)//2),WELCOME_TEXT,curses.A_UNDERLINE | curses.A_BOLD)
+    stdscr.addstr(2,center_x-(len("Select something, press enter for select and < - > for navigate")//2),"Select something, press enter for select and < - > for navigate")
+    #creating the function seletction menu
+    for i in range(len(FUNCTION_LIST)):
+        seqments = cols//(1+len(FUNCTION_LIST))
+        #caculating the perfect middle for each text selection
+        x = seqments*(i+1)-(len(FUNCTION_LIST)//2)
+        stdscr.addstr(4,x,FUNCTION_LIST[i])
     
-    for i in range(1000):
-        color = GREEN_AND_BLUE
-        stdscr.clear()
-        if i %2 == 0:
-            color = GREEN_AND_BLUE | curses.A_REVERSE
+    #selection module
+    selected = 0
+    while (key := stdscr.getch()) != 113:
+        seqments = cols//(1+len(FUNCTION_LIST))
+        if key == 260:#links
+            #setting selected befor back to normal
+            stdscr.addstr(4,x,FUNCTION_LIST[selected % len(FUNCTION_LIST)])
             
-        stdscr.addstr(f"Count: {i}",color)
-        time.sleep(.0000000000000001)
+            selected = selected - 1
+            
+            #calculate postionx
+            x = seqments*(selected % len(FUNCTION_LIST) + 1)-(len(FUNCTION_LIST)//2)
+            #marker the new selected
+            stdscr.addstr(4,x,FUNCTION_LIST[selected % len(FUNCTION_LIST)],curses.A_REVERSE)
+        if key == 261:#rechts
+            #setting selected befor back to normal
+            stdscr.addstr(4,x,FUNCTION_LIST[selected % len(FUNCTION_LIST)])
+            
+            selected = selected + 1
+            
+            #calculate postionx
+            x = seqments*(selected % len(FUNCTION_LIST) + 1)-(len(FUNCTION_LIST)//2)
+            #marker the new selected
+            stdscr.addstr(4,x,FUNCTION_LIST[selected % len(FUNCTION_LIST)],curses.A_REVERSE)
         stdscr.refresh()
-    stdscr.getch()
-    
-wrapper(main)
-"""
+    print(key)
 
-# Import the curses module and initialize it
-import curses
 
-# initialize curses
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
 
-# Set up the user interface and create the main window
-stdscr.addstr(0, 0, "Welcome to my terminal-based editor!")
-stdscr.addstr(1, 0, "Type ':q' to quit.")
 
-# create the main window
-editor_win = curses.newwin(curses.LINES - 2, curses.COLS, 2, 0)
-editor_win.scrollok(True)
+wrapper(menu)
 
-# Define the main loop for the editor
-stdscr.addstr(10, 20, "Hallo mein Schatz",curses.A_UNDERLINE | curses.A_BOLD)
-stdscr.addstr(15, 40, "Hallo mein Schatz",curses.A_UNDERLINE | curses.A_BOLD)
-stdscr.getch()
-
-# Clean up and exit the program
-curses.nocbreak()
-curses.echo()
-curses.endwin()
